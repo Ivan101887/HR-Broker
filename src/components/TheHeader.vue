6238674168cefc62a952ee0f77f81d80d45291c7
Ivan yuan <Ivan_Yuan@cmoney.com.tw>
2022-05-04 13:16:50 +0800

    first commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <figure class="logo">
+      <img src="@/assets/logo.png" alt="F2E 前端開發" class="logo__img" />
+    </figure>
+    <nav>
+      <router-link to="/">首頁</router-link>
+      <router-link to="/members">會員列表</router-link>
+      <router-link to="/custom">自選清單</router-link>
+    </nav>
+    <input type="button" value="登入" class="btn btn-peace" @click="$emit('switch')">
+  </header>
+</template>
+<script>
+export default {
+  name: 'the-header',
+};
+</script>
---
+++ src/views/CustomView.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>這裡是自選清單</div>
+</template>
